c99bb270a2fdef535ca433773dd369ff9d1bb01d
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-03-16 23:55:13 +0200

    Create templates for views

--- blog_app/models.py
+++ blog_app/models.py
@@ -42,7 +42,7 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse(
-            'blog:post_detail',
+            'blog_app:post_detail',
             args=[
                 self.publish.year, self.publish.month,
                 self.publish.day, self.slug
