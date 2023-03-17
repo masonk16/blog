ad03aae1b85b97592f51577071c38bf0b7bd06dd
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-03-17 21:53:50 +0200

    Add your Comment model to the administration site

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Post
+from .models import User, Post, Comment
 
 admin.site.register(User, UserAdmin)
 
@@ -14,3 +14,10 @@ class PostAdmin(admin.ModelAdmin):
     raw_id_fields = ("author",)
     date_hierarchy = "publish"
     ordering = ("status", "publish")
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
