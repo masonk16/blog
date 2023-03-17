b64c71793cdb39090d8a9cd5faaf9b8d6f323dad
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-03-17 22:49:35 +0200

    Add taggit tag manager.

--- blog_app/models.py
+++ blog_app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import AbstractUser
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 
 class User(AbstractUser):
@@ -30,6 +31,8 @@ class Post(models.Model):
     status = models.CharField(
         max_length=10, choices=STATUS_CHOICES, default="draft"
     )
+
+    tags = TaggableManager()
 
     objects = models.Manager()  # The default manager.
     published = PublishedManager()  # Our custom manager.
