8a454a6a14c9abbaf27b0558014231701ab18ea2
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-03-19 00:12:13 +0200

    Add search url and search button to base template.

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -16,5 +16,6 @@ urlpatterns = [
          name='post_share'),
     path('tag/<slug:tag_slug>/', views.post_list,
          name='post_list_by_tag'),
-    path('feed/', LatestPostsFeed(), name='post_feed')
+    path('feed/', LatestPostsFeed(), name='post_feed'),
+    path('search/', views.post_search, name='post_search')
 ]
