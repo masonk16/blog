9773acfc78d0ab91bfadde443140aa6cae7f1527
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-03-18 21:40:41 +0200

    Adding sitemaps.

--- blog/urls.py
+++ blog/urls.py
@@ -17,8 +17,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from blog_app.sitemaps import PostSitemap
+
+sitemaps = {
+    'posts': PostSitemap
+}
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('blog/', include('blog_app.urls', namespace='blog_app')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+         name='django.contrib.sitemaps.views.sitemap')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
