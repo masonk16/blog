855bdfdf0760e91812bdb02be20fb28fe726fdbc
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-03-18 21:57:03 +0200

    Creating feeds for blog posts

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .feeds import LatestPostsFeed
 
 
 app_name = 'blog_app'
@@ -15,4 +16,5 @@ urlpatterns = [
          name='post_share'),
     path('tag/<slug:tag_slug>/', views.post_list,
          name='post_list_by_tag'),
+    path('feed/', LatestPostsFeed(), name='post_feed')
 ]
